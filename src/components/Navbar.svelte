48396aa4278b1edc84e3c9efc44f2dd3b8bff8a6
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-10-11 10:50:26 +0200

    Updated Navbar to highlight buttons based on active path.

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { page } from "$app/stores"
 	import { user } from "../stores/authStore"
 	import { supabase } from "../supabaseClient"
 
@@ -12,11 +13,15 @@
             location.reload()
         }
     }
+    
+    $: isActive = (path: string) => $page.url.pathname === path ? 'bg-blue-500' : 'bg-blue-400'
+
 </script>
 
 <div class="flex justify-center px-4 my-4">
-    <a href="/"><button class="shadow rounded bg-blue-400 hover:bg-blue-500 text-white py-1 px-2 mx-2">Home</button></a>
-    <a href="/about"><button class="shadow rounded bg-blue-400 hover:bg-blue-500 text-white py-1 px-2 mx-2">About</button></a>
+    <a href="/"><button class={`shadow rounded ${isActive('/')} bg-blue-400 hover:bg-blue-500 text-white py-1 px-2 mx-2`}>Home</button></a>
+
+    <a href="/about"><button class={`shadow rounded ${isActive("/about")} bg-blue-400 hover:bg-blue-500 text-white py-1 px-2 mx-2`}>About</button></a>
     {#if $user}
         <button on:click={logout} class="shadow rounded bg-blue-400 hover:bg-blue-500 text-white py-1 px-2 mx-2">Log out</button>
     {/if}
